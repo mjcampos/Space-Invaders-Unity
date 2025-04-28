a8cb3545a74259e9083c9231c6a39aecb91f9f58
Michael Campos <mjcampos@users.noreply.github.com>
2025-04-28 15:15:44 +0200

    Setup enemy collision to destroy upon contact with anything

--- Assets/Scripts/EnemyCollider.cs
+++ Assets/Scripts/EnemyCollider.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 enum Size {
     Small = 1,
@@ -7,30 +8,36 @@ enum Size {
     Large = 3
 }
 
-public class EnemyCollider : MonoBehaviour
-{
-    [SerializeField] GameObject explosion;
+public class EnemyCollider : MonoBehaviour {
+    [FormerlySerializedAs("explosion")] [SerializeField] GameObject explosionAnim;
     [SerializeField] Size size;
     [SerializeField] GameObject explosionSound;
     
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        // Instantiate a temporary audio source at the enemy's position
+    void OnTriggerEnter2D(Collider2D other) {
+        /*
+         * When an enemy hits another object these events will happen
+         * 1. Generate a explosion sound
+         *      a. Destroy temp audio after sound finishes
+         * 2. Generate an explosion animation
+         * 3. Destroy this enemy object
+         */
+        
+        // Step 1
         GameObject tempAudio = Instantiate(explosionSound, transform.position, Quaternion.identity);
         AudioSource audioSource = tempAudio.GetComponent<AudioSource>();
-
+        
         audioSource.Play();
         
-        // Destroy temp audio object after sound finishes
+        // Step 1 - a
         Destroy(tempAudio, audioSource.clip.length);;
         
-        if (other.gameObject.CompareTag("Laser"))
-        {
-            GameObject explosionInstance = Instantiate(explosion, transform.position, Quaternion.identity);
+        // Step 2
+        GameObject explosionInstance = Instantiate(explosionAnim, transform.position, Quaternion.identity);
 
-            explosionInstance.transform.localScale = Vector3.one * (int)size;
-            explosionInstance.transform.SetParent(null);
-            Destroy(gameObject);
-        }
+        explosionInstance.transform.localScale = Vector3.one * (int)size;
+        explosionInstance.transform.SetParent(null);
+        
+        // Step 3
+        Destroy(gameObject);
     }
 }
