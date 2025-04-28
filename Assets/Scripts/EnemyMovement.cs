180bb8892781015b14f51b8e9d534acea9c39076
Michael Campos <mjcampos@users.noreply.github.com>
2025-04-28 12:52:57 +0200

    Modified movement
    Replaced dynamic spawn generator with static enemies to make it easier to make them move as a single entity without the risk of cross the borders.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
-{
+public class EnemyMovement : MonoBehaviour {
     [Header("Movement Settings")] 
     [SerializeField] float initialMoveDelay = 1.0f;
     [SerializeField] float moveDistance = 0.2f;
     [SerializeField] float dropDistance = 0.5f;
     [SerializeField] float moveInterval = 1.0f;
     [SerializeField] float accelerationRate = 0.01f;  // How much faster after each move
-
+    
     [Header("Sound Settings")] 
     [SerializeField] AudioClip[] moveSounds;
     
@@ -17,85 +16,74 @@ public class EnemyMovement : MonoBehaviour
     
     bool _movingRight = true;
     float _moveTimer;
-    Camera _camera;
-    
-    EnemySpawner _enemySpawner;
+    float _rightEdge = 3.0f;
+    float _leftEdge = -3.0f;
+
+    enum  State {
+        Move,
+        Drop
+    }
+
+    State _nextMove;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // Before we even consider moving the group we need to ensure they spawn before anything else
-        _enemySpawner = GetComponent<EnemySpawner>();
-        
-        _enemySpawner.StartSpawning();
-        
-        // Initialize everything
-        _camera = GameObject.Find("Camera").GetComponent<Camera>();
+    void Start() {
         _audioSource = GetComponent<AudioSource>();
         
         _moveTimer = initialMoveDelay;
+        _nextMove = State.Move;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         _moveTimer -= Time.deltaTime;
 
-        if (_moveTimer <= 0f)
-        {
+        if (_moveTimer <= 0f) {
             MoveEnemies();
             _moveTimer = moveInterval;
         }
     }
 
-    void MoveEnemies()
-    {
-        Vector3 moveDirection = _movingRight ? Vector3.right : Vector3.left;
-        
-        transform.position += moveDirection * moveDistance;
+    void MoveEnemies() {
+        /*
+         * When this method gets called the enemies will do either:
+         * 1. Move
+         * 2. Drop
+         */
 
+        switch (_nextMove) {
+            case State.Move:
+                Vector3 moveDirection = _movingRight ? Vector3.right : Vector3.left;
+                
+                transform.Translate(moveDirection * moveDistance);
+                
+                float xPos = transform.position.x;
+                float clampedX = Mathf.Clamp(xPos, _leftEdge, _rightEdge);
+                transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+                
+                _nextMove = (transform.position.x >= _rightEdge || transform.position.x <= _leftEdge) ? State.Drop : State.Move;
+                
+                break;
+            
+            case State.Drop:
+                transform.Translate(Vector3.down * dropDistance);
+                _movingRight = !_movingRight;  // Flip direction
+                _nextMove = State.Move;
+                break;
+        }
+        
+        
         PlayMoveSound();
         
         // Smoothly accelerate movement
         moveInterval = Mathf.Max(0.1f, moveInterval - accelerationRate);
-
-        if (ReachedScreenEdge())
-        {
-            _movingRight = !_movingRight;  // Flip direction
-            transform.position += Vector3.down * dropDistance;  // Drop down
-        }
     }
-
-    void PlayMoveSound()
-    {
-        if (moveSounds.Length > 0)
-        {
+    
+    void PlayMoveSound() {
+        if (moveSounds.Length > 0) {
             _audioSource.PlayOneShot(moveSounds[_currentMoveSoundIndex]);
             
             _currentMoveSoundIndex = (_currentMoveSoundIndex + 1) % moveSounds.Length;
         }
-    }
-    
-    bool ReachedScreenEdge()
-    {
-        foreach (Transform enemy in transform)
-        {
-            if (!enemy.gameObject.activeInHierarchy)
-            {
-                continue;
-            }
-
-            Vector3 enemyViewportPos = _camera.WorldToViewportPoint(enemy.position);
-
-            if (_movingRight && enemyViewportPos.x >= 0.95f)  // Near right edge
-            {
-                return true;
-            } else if (!_movingRight && enemyViewportPos.x <= 0.05f)  // Near left edge
-            {
-                return true;
-            }
-        }
-        
-        return false;
     }
 }
