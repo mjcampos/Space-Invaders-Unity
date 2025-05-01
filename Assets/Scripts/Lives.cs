c34b11d729db53fd8ebe3d44df4e54e457cd1e8c
Michael Campos <mjcampos@users.noreply.github.com>
2025-05-01 21:59:20 +0200

    Add gamepad rumble
    If the player is playing with a gamepad give it a rumble whenever the player loses a life.

--- Assets/Scripts/Lives.cs
+++ Assets/Scripts/Lives.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Lives : MonoBehaviour {
     int _lives = 3;
+    Gamepad _gamepad;
+
+    void Start()
+    {
+        _gamepad = Gamepad.current;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        // If player has gamepad, give them a rumble
+        GiveGamePadARumble();
+        
         // Decrement a life
         _lives--;
         
@@ -22,5 +32,31 @@ public class Lives : MonoBehaviour {
     {
         yield return new WaitForSeconds(0.5f);
         GameManager.Instance.PlayerLost();
+    }
+
+    IEnumerator StopRumbleAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _gamepad.SetMotorSpeeds(0, 0);
+    }
+
+    void GiveGamePadARumble()
+    {
+        if (_gamepad != null)
+        {
+            // Set vibration: LeftMotor (low freq), rightMotor (high freq)
+            _gamepad.SetMotorSpeeds(0.4f, 0.8f);
+            
+            // Stop vibration after short delay
+            StartCoroutine(StopRumbleAfterDelay(0.4f));
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_gamepad != null)
+        {
+            _gamepad.SetMotorSpeeds(0, 0);
+        }
     }
 }
